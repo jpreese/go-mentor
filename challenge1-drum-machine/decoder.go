15c878b4fc3636cebc26ab7e229e1718505dd772
John Reese <john@reese.dev>
2019-10-03 16:00:48 -0400

    Move DecodeFile to drum

--- challenge1-drum-machine/decoder.go
+++ challenge1-drum-machine/decoder.go
@@ -5,7 +5,6 @@ import (
 	"encoding/binary"
 	"fmt"
 	"io"
-	"os"
 )
 
 // NOTE TO REVIEWER:
@@ -26,42 +25,6 @@ type track struct {
 	ID    int
 	Name  string
 	Steps []byte
-}
-
-// DecodeFile decodes the drum machine file found at the provided path
-// and returns a pointer to a parsed pattern which is the entry point to the
-// rest of the data.
-func DecodeFile(path string) (*pattern, error) {
-	file, err := os.Open(path)
-	if err != nil {
-		return nil, err
-	}
-	defer file.Close()
-
-	p := pattern{}
-
-	err = p.readHeader(file)
-	if err != nil {
-		return nil, fmt.Errorf("Unable to read file header")
-	}
-
-	for {
-		offset, err := file.Seek(0, os.SEEK_CUR)
-		if err != nil {
-			return nil, fmt.Errorf("Unable to determine current seek position")
-		}
-
-		if offset > p.fileSize {
-			break
-		}
-
-		err = p.readTrack(file)
-		if err != nil {
-			return nil, fmt.Errorf("Unable to read track")
-		}
-	}
-
-	return &p, nil
 }
 
 func (p *pattern) readHeader(file io.Reader) error {
--- challenge1-drum-machine/drum.go
+++ challenge1-drum-machine/drum.go
@@ -1,3 +1,42 @@
-// Package drum is supposed to implement the decoding of .splice drum machine files.
-// See golang-challenge.com/go-challenge1/ for more information
 package drum
+
+import (
+	"fmt"
+	"os"
+)
+
+// DecodeFile decodes the drum machine file found at the provided path
+// and returns a pointer to a parsed pattern which is the entry point to the
+// rest of the data.
+func DecodeFile(path string) (*pattern, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	p := pattern{}
+
+	err = p.readHeader(file)
+	if err != nil {
+		return nil, fmt.Errorf("Unable to read file header")
+	}
+
+	for {
+		offset, err := file.Seek(0, os.SEEK_CUR)
+		if err != nil {
+			return nil, fmt.Errorf("Unable to determine current seek position")
+		}
+
+		if offset > p.fileSize {
+			break
+		}
+
+		err = p.readTrack(file)
+		if err != nil {
+			return nil, fmt.Errorf("Unable to read track")
+		}
+	}
+
+	return &p, nil
+}
